ff13f4748928f3a0097b29c3e029939e427e5625
kareem alaa <kareemalaanaga98@gmail.com>
2024-03-13 15:43:51 +0200

    first commit

---
+++ Hospital Appointment/Controllers/DoctorController.cs
@@ -0,0 +1,23 @@
+﻿using Hospital_Appointment.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hospital_Appointment.Controllers
+{
+    public class DoctorController : Controller
+    {
+        public IActionResult BookAppointment()
+        {
+            SampleDataDoctor sample = new SampleDataDoctor();
+
+            var results = sample.doctors.ToList();
+            return View(results);
+        }
+
+        public IActionResult CompleteAppointment(int id)
+        {
+            SampleDataDoctor sample = new SampleDataDoctor();
+           
+            return View(sample.doctors.First(e => e.Id == id));
+        }
+    }
+}
---
+++ Hospital Appointment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hospital_Appointment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
